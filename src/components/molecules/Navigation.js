fd2af2efe855945ac3e8ba649958b11b3aaf8a8a
Will McLean <willmcl@gmail.com>
2020-02-08 10:53:55 +1100

    Remove things that we don't need.

--- src/components/molecules/Navigation.js
+++ src/components/molecules/Navigation.js
@@ -7,8 +7,7 @@ class Navigation extends Component {
       <nav className="Navigation">
         <ul>
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/about/">About</Link></li>
-          <li><Link to="/posts/">Posts</Link></li>
+          <li><Link to="/strategy/">Strategy</Link></li>
         </ul>
       </nav>
     )
---
+++ src/pages/strategy.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SEO from "../components/molecules/SEO"
+
+const Strategy = () => (
+  <>
+    <SEO title="Strategy" />
+    <h1>Strategy</h1>
+    <p>Strategy page</p>
+  </>
+);
+
+export default Strategy;
