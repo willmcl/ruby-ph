8cc7c56f3e0b18ee788d0ebf8fbedfadb7c4436d
LuminousNose <quosimosaur@gmail.com>
2020-02-09 22:26:17 +1100

    - Split HomeNavigation into ColourNavigation and TextNavigation - styling TextNavigation - styling ColourNavigation for index and pages - add ColourNavigation to PageStructure

--- src/components/molecules/Footer.js
+++ src/components/molecules/Footer.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import ColourNavigation from '../molecules/ColourNavigation';
 
 const Holder = styled.footer`
+	@media( ${props => props.theme.breakpoints.sm} ){
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: 100px 100px;
+		grid-template-areas:
+			"colournav ."
+			"textfooter textfooter";
+  }
 
+  .colournav {
+  	grid-area: colournav;
+  }
+
+  .textfooter {
+  	grid-area: textfooter;
+  }
 `
 
 const TextFooter = styled.div`
@@ -14,7 +30,8 @@ class Footer extends Component {
   render() {
     return ( 
   		<Holder className="Footer">
-  			<TextFooter>	
+  			<ColourNavigation classProp="colournav" />
+  			<TextFooter className="textfooter">	
 	        <p>Contact — Archive — Instagram — Youtube — Shop</p>
 	        <p>Copyright Ruby PH {new Date().getFullYear()} — Coded by <a href="https://www.willmclean.net">Will McLean</a></p>
 	        </TextFooter>
